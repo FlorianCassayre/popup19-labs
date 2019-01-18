81e3f2a29fadc9e57d54a18e9e11a91e6882a506
Florian Cassayre <florian.cassayre@gmail.com>
2019-01-18 15:59:31 +0100

    First half done (decider for impossible cases).

--- src/main/java/se/kth/popup/intervalcover/IntervalUtils.java
+++ src/main/java/se/kth/popup/intervalcover/IntervalUtils.java
@@ -1,7 +1,6 @@
 package se.kth.popup.intervalcover;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public final class IntervalUtils {
     private IntervalUtils() {}
@@ -14,6 +13,35 @@ public final class IntervalUtils {
      */
     public static Set<Integer> cover(Interval interval, Interval[] intervals) {
 
-        return new HashSet<>(); // TODO
+        Arrays.sort(intervals, Comparator.comparingDouble(i -> i.a)); // Sort the intervals by their lower bound, O(n log n)
+
+        final Set<Integer> set = new HashSet<>();
+
+        if(intervals.length == 0) // Impossible (no intervals to cover with)
+            return set;
+
+        // Check if it is possible
+        int lastBelow = -1;
+        for(int i = 0; i < intervals.length; i++) {
+            if(intervals[i].a <= interval.a)
+                lastBelow = i;
+            else
+                break;
+        }
+        if(lastBelow == -1)
+            return set; // Impossible (cannot cover left side)
+
+        double max = 0;
+        for(int i = lastBelow; i < intervals.length && max < interval.b; i++) {
+            if(i > 0 && intervals[i].a > max)
+                break;
+            max = Math.max(intervals[i].b, max);
+        }
+        if(max < interval.b) // Impossible (cannot cover right side)
+            return set;
+
+        // TODO actually compute the min. interval cover
+
+        return new HashSet<>(Arrays.asList(0)); // FIXME
     }
 }
