c7a8beb322c971a0ffa84194b0a0ee03326f3570
Florian Cassayre <florian.cassayre@gmail.com>
2019-01-18 17:19:13 +0100

    Should be working when fixed, see FIXME.

--- src/main/java/se/kth/popup/intervalcover/IntervalUtils.java
+++ src/main/java/se/kth/popup/intervalcover/IntervalUtils.java
@@ -1,3 +1,10 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
 package se.kth.popup.intervalcover;
 
 import java.util.*;
@@ -13,6 +20,7 @@ public final class IntervalUtils {
      */
     public static Set<Integer> cover(Interval interval, Interval[] intervals) {
 
+        // FIXME preserve the indices before sorting!
         Arrays.sort(intervals, Comparator.comparingDouble(i -> i.a)); // Sort the intervals by their lower bound, O(n log n)
 
         final Set<Integer> set = new HashSet<>();
@@ -21,27 +29,47 @@ public final class IntervalUtils {
             return set;
 
         // Check if it is possible
-        int lastBelow = -1;
-        for(int i = 0; i < intervals.length; i++) {
-            if(intervals[i].a <= interval.a)
-                lastBelow = i;
-            else
-                break;
+        double largestLeft = 0, largestRight = 0;
+        int largestLeftIndex = -1, largestRightIndex = -1;
+        for(int i = 0; i < intervals.length && intervals[i].a <= interval.a; i++) {
+            if(intervals[i].b >= interval.a && (largestLeftIndex == -1 || intervals[i].b > largestLeft)) {
+                largestLeft = intervals[i].b;
+                largestLeftIndex = i;
+            }
         }
-        if(lastBelow == -1)
+        if(largestLeftIndex == -1)
             return set; // Impossible (cannot cover left side)
 
-        double max = 0;
-        for(int i = lastBelow; i < intervals.length && max < interval.b; i++) {
-            if(i > 0 && intervals[i].a > max)
-                break;
-            max = Math.max(intervals[i].b, max);
+        for(int i = intervals.length - 1; i >= 0 && intervals[i].b >= interval.b; i--) {
+            if(intervals[i].b >= interval.b && (largestRightIndex == -1 || intervals[i].a < largestRight)) {
+                largestRight = intervals[i].b;
+                largestRightIndex = i;
+            }
         }
-        if(max < interval.b) // Impossible (cannot cover right side)
-            return set;
+        if(largestRightIndex == -1)
+            return set; // Impossible (cannot cover right side)
 
-        // TODO actually compute the min. interval cover
+        int index = largestLeftIndex, start = largestLeftIndex + 1;
+        set.add(largestLeftIndex);
+        while(intervals[index].b < interval.b) {
+            double bestValue = 0;
+            int bestIndex = -1;
 
-        return new HashSet<>(Arrays.asList(0)); // FIXME
+            for(int i = start; i < intervals.length && intervals[i].a <= intervals[index].b; i++) {
+                start = i + 1;
+                if(intervals[i].b >= intervals[index].a && (bestIndex == -1 || intervals[i].b > bestValue)) {
+                    bestValue = intervals[i].b;
+                    bestIndex = i;
+                }
+            }
+
+            if(bestIndex == -1) // Impossible (no intervals were found)
+                return new HashSet<>();
+
+            set.add(bestIndex);
+            index = bestIndex;
+        }
+
+        return set;
     }
 }
--- src/test/java/se/kth/popup/intervalcover/IntervalCoverTest.java
+++ src/test/java/se/kth/popup/intervalcover/IntervalCoverTest.java
@@ -1,11 +1,37 @@
 package se.kth.popup.intervalcover;
 
+import org.junit.jupiter.api.Test;
 import se.kth.popup.KattisTester;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IntervalCoverTest extends KattisTester {
 
-    public IntervalCoverTest() {
+    IntervalCoverTest() {
         super("intervalcover", IntervalCover::run);
     }
 
+    @Test
+    void testEdgeCases() {
+        assertTrue(IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] {}).isEmpty());
+        assertEquals(set(0), IntervalUtils.cover(new Interval(0.0, 0.0), new Interval[] { new Interval(-1.0, 1.0) }));
+        assertEquals(set(), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(-1.0, 0.5), new Interval(0.4, 0.6) }));
+        assertEquals(set(2), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] {
+                new Interval(-100.0, -99.0), new Interval(-2.0, 0.5), new Interval(-1.0, 1.0), new Interval(0.5, 2.0), new Interval(-0.9, 0.9), new Interval(99.0, 100.0)
+        }));
+        assertEquals(set(), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(-100.0, 0.0) }));
+        assertEquals(set(), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(-100.0, 0.5) }));
+        assertEquals(set(0), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(-100.0, 1.0) }));
+        assertEquals(set(), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(0.5, 100.0) }));
+        assertEquals(set(0), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(0.0, 100.0) }));
+        assertEquals(set(0, 1), IntervalUtils.cover(new Interval(0.0, 1.0), new Interval[] { new Interval(-1.0, 0.5), new Interval(0.5, 1.0), new Interval(-100.0, 0.99) }));
+    }
+
+    <T> Set<? extends T> set(T... elements) {
+        return new HashSet<>(Arrays.asList(elements));
+    }
 }
