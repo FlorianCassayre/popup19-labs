8de8e64c938ec3082d2da5395ed2a6ff8f8dd504
Florian Cassayre <florian.cassayre@gmail.com>
2019-01-20 16:38:26 +0100

    Knapsack implementation idea.

--- src/main/java/se/kth/popup/knapsack/KnapsackMain.java
+++ src/main/java/se/kth/popup/knapsack/KnapsackMain.java
@@ -1,3 +1,10 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
 package se.kth.popup.knapsack;
 
 import se.kth.popup.Kattio;
@@ -9,20 +16,23 @@ public class KnapsackMain {
 
     public static void run(Kattio kattio) {
 
-        final int capacity = (int) kattio.getDouble();
-        final int n = kattio.getInt();
+        while(kattio.hasMoreTokens()) {
+            final int capacity = (int) kattio.getDouble();
+            final int n = kattio.getInt();
 
-        final int[] values = new int[n], weights = new int[n];
+            final int[] values = new int[n], weights = new int[n];
 
-        for(int i = 0; i < n; i++) {
-            values[i] = kattio.getInt();
-            weights[i] = kattio.getInt();
+            for(int i = 0; i < n; i++) {
+                values[i] = kattio.getInt();
+                weights[i] = kattio.getInt();
+            }
+
+            final Set<Integer> set = KnapsackUtils.knapsack(capacity, values, weights);
+
+            kattio.println(set.size());
+            kattio.println(set.stream().sorted().map(String::valueOf).collect(Collectors.joining(" ")));
         }
 
-        final Set<Integer> set = KnapsackUtils.knapsack(capacity, values, weights);
-
-        kattio.println(set.size());
-        kattio.println(set.stream().map(String::valueOf).collect(Collectors.joining(" ")));
 
         kattio.close();
     }
--- src/main/java/se/kth/popup/knapsack/KnapsackUtils.java
+++ src/main/java/se/kth/popup/knapsack/KnapsackUtils.java
@@ -1,6 +1,13 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
 package se.kth.popup.knapsack;
 
-import java.util.Set;
+import java.util.*;
 
 public final class KnapsackUtils {
     private KnapsackUtils() {}
@@ -19,9 +26,38 @@ public final class KnapsackUtils {
 
         final int n = values.length;
 
+        final List<Set<Integer>> solutions = new ArrayList<>(capacity + 1);
+        final int[] solutionsValues = new int[capacity + 1];
 
+        // Base case
+        solutions.add(new HashSet<>());
+        solutionsValues[0] = 0;
 
+        for(int c = 1; c <= capacity; c++) {
 
-        return null; // TODO
+            Set<Integer> bestSet = solutions.get(c - 1);
+            int bestValue = solutionsValues[c - 1];
+
+            for(int i = 0; i < n; i++) {
+                final int previousWeight = c - weights[i];
+                if(previousWeight >= 0) { // Item can enter a knapsack of capacity `c`
+                    final Set<Integer> set = solutions.get(previousWeight);
+                    if(!set.contains(i)) { // This knapsack does not contain the item, yet
+                        final int value = solutionsValues[previousWeight] + values[i];
+                        if(value > bestValue) { // FIXME potentially greedy!!
+                            bestSet = new HashSet<>(set);
+                            bestSet.add(i);
+
+                            bestValue = value;
+                        }
+                    }
+                }
+            }
+
+            solutions.add(bestSet);
+            solutionsValues[c] = bestValue;
+        }
+
+        return solutions.get(capacity);
     }
 }
