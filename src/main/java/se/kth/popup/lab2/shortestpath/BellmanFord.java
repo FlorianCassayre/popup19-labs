f2c1173696b6abd987c395d730658de67744f45d
Florian Cassayre <florian.cassayre@gmail.com>
2019-02-17 16:57:54 +0100

    Bogus Bellman-Ford.

---
+++ src/main/java/se/kth/popup/lab2/shortestpath/BellmanFord.java
@@ -0,0 +1,73 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
+package se.kth.popup.lab2.shortestpath;
+
+import java.util.List;
+
+public final class BellmanFord {
+    private BellmanFord() {}
+
+    /**
+     * Computes all the shortest paths from a single source.
+     * Negative weights are allowed, and negative cycle will be detected.
+     * @param n the number of vertices
+     * @param edges the edges
+     * @param s the source vertex
+     * @return a solution containing the costs and the paths;
+     * a distance of <code>Integer.MAX_VALUE</code> indicates an impossible path
+     * while <code>Integer.MAX_VALUE</code> means that there is a negatives cycle
+     */
+    public static Solution shortestPath(int n, List<Edge> edges, int s) {
+        final int[] distances = new int[n], parents = new int[n];
+
+        // Initialization
+        for(int i = 0; i < n; i++) {
+            distances[i] = Integer.MAX_VALUE;
+            parents[i] = -1;
+        }
+
+        distances[s] = 0; // Source
+
+        // Relaxation
+        for(int i = 0; i < n - 1; i++)
+            for(Edge edge : edges)
+                if(distances[edge.u] < Integer.MAX_VALUE && distances[edge.u] + edge.weight < distances[edge.v]) {
+                    distances[edge.v] = distances[edge.u] + edge.weight;
+                    parents[edge.v] = edge.u;
+                }
+
+        // Negative cycles
+        for(Edge edge : edges)
+            if(distances[edge.u] < Integer.MAX_VALUE && distances[edge.u] + edge.weight < distances[edge.v])
+                distances[edge.u] = Integer.MIN_VALUE;
+
+        return new Solution(n, distances, parents);
+    }
+
+    public static final class Solution {
+        public final int n;
+        public final int[] distances, parents;
+
+        private Solution(int n, int[] distances, int[] parents) {
+            this.n = n;
+            this.distances = distances;
+            this.parents = parents;
+        }
+    }
+
+    public static final class Edge {
+        public final int u, v;
+        public final int weight;
+
+        public Edge(int u, int v, int weight) {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
+    }
+}
---
+++ src/main/java/se/kth/popup/lab2/shortestpath/NegativeWeightsMain.java
@@ -0,0 +1,56 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
+package se.kth.popup.lab2.shortestpath;
+
+import se.kth.popup.Kattio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NegativeWeightsMain {
+    public static void run(Kattio kattio) {
+        boolean first = true;
+        while(true) {
+            if(!first)
+                kattio.println();
+            first = !first;
+
+            final int n = kattio.getInt(), m = kattio.getInt(), q = kattio.getInt(), s = kattio.getInt();
+
+            if(n == 0 && m == 0 && q == 0 && s == 0)
+                break;
+
+            final List<BellmanFord.Edge> edges = new ArrayList<>(m);
+            for(int i = 0; i < m; i++) {
+                final int u = kattio.getInt(), v = kattio.getInt(), w = kattio.getInt();
+                edges.add(new BellmanFord.Edge(u, v, w));
+            }
+
+            final BellmanFord.Solution solution = BellmanFord.shortestPath(n, edges, s);
+
+            for(int i = 0; i < q; i++) {
+                final int to = kattio.getInt();
+
+                final int distance = solution.distances[to];
+                if(distance == Integer.MIN_VALUE) {
+                    kattio.println("-Infinity");
+                } else if(distance == Integer.MAX_VALUE) {
+                    kattio.println("Impossible");
+                } else {
+                    kattio.println(distance);
+                }
+            }
+        }
+
+        kattio.close();
+    }
+
+    public static void main(String[] args) {
+        run(new Kattio());
+    }
+}
