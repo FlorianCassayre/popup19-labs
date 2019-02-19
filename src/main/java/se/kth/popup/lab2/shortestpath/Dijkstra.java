56cf7d4b819044bbcf6f3f2bea15c1420057bb78
Florian Cassayre <florian.cassayre@gmail.com>
2019-02-19 19:15:23 +0100

    Fixed Dijkstra (single source, positive edges).

--- src/main/java/se/kth/popup/lab2/shortestpath/Dijkstra.java
+++ src/main/java/se/kth/popup/lab2/shortestpath/Dijkstra.java
@@ -1,44 +1,57 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
 package se.kth.popup.lab2.shortestpath;
-
-import se.kth.popup.lab2.MinHeap;
 
 import java.util.*;
 
 public final class Dijkstra {
     private Dijkstra() {}
 
+    /**
+     * Finds the shortest paths from a single source using Dijkstra.
+     * @param n the number of vertices
+     * @param edges the weighted directed edges
+     * @param s the source vertex
+     * @return the shortest distances to each target if it exists,
+     * or <code>Integer.MAX_VALUE</code> otherwise; as well as the
+     * parent vertex or <code>-1</code> if it is the root of the tree
+     * or if there exist no shortest path to that vertex.
+     */
     public static Solution shortestPath(int n, List<Edge> edges, int s) {
-        final Map<Integer, List<Edge>> map = new HashMap<>();
+        // (similar to MST, Dijkstra with a priority queue)
+
+        final List<List<Edge>> adjacency = new ArrayList<>(n);
         for(int i = 0; i < n; i++)
-            map.put(i, new ArrayList<>());
+            adjacency.add(new ArrayList<>());
 
-        edges.forEach(e -> {
-            map.get(e.u).add(e);
-        });
+        for(Edge edge : edges)
+            adjacency.get(edge.u).add(edge);
 
-        final int[] distances = new int[n];
-        final int[] parents = new int[n];
-        for(int i = 0; i < distances.length; i++) {
+        final int[] distances = new int[n], parents = new int[n];
+        for(int i = 0; i < n; i++) {
             distances[i] = Integer.MAX_VALUE;
             parents[i] = -1;
         }
 
-        final MinHeap<Integer> queue = new MinHeap<>(Comparator.comparingLong(l -> distances[l]));
-        for(int i = 0; i < n; i++)
-            queue.add(i);
+        final PriorityQueue<Vertex> queue = new PriorityQueue<>();
 
-        distances[s] = 0;
+        queue.add(new Vertex(s, -1, 0));
 
         while(!queue.isEmpty()) {
-            final int u = queue.poll();
-            map.get(u).forEach(edge -> {
-                final int v = edge.v;
-                if(distances[u] < Integer.MAX_VALUE && distances[v] > distances[u] + edge.weight) {
-                    distances[v] = distances[u] + edge.weight;
-                    parents[v] = u;
-                    queue.decreaseKeyElement(v);
-                }
-            });
+            final Vertex vertex = queue.poll();
+
+            if(distances[vertex.v] == Integer.MAX_VALUE && vertex.distance < distances[vertex.v]) {
+                distances[vertex.v] = vertex.distance;
+                parents[vertex.v] = vertex.parent;
+
+                for(Edge edge : adjacency.get(vertex.v))
+                    queue.add(new Vertex(edge.v, edge.u, vertex.distance + edge.weight));
+            }
         }
 
         return new Solution(n, distances, parents);
@@ -52,6 +65,22 @@ public final class Dijkstra {
             this.n = n;
             this.distances = distances;
             this.parents = parents;
+        }
+    }
+
+    private static final class Vertex implements Comparable<Vertex> {
+        private final int v, parent;
+        private final int distance;
+
+        private Vertex(int v, int parent, int distance) {
+            this.v = v;
+            this.parent = parent;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Vertex that) {
+            return Integer.compare(this.distance, that.distance);
         }
     }
 
