30547b828cdedbf2df21dfe4e9df4d94cbbb8f45
Florian Cassayre <florian.cassayre@gmail.com>
2019-02-24 17:55:37 +0100

    Floyd-Warshall now passing. The problem was because of the presence of multi-edges...

--- src/main/java/se/kth/popup/lab2/shortestpath/FloydWarshall.java
+++ src/main/java/se/kth/popup/lab2/shortestpath/FloydWarshall.java
@@ -1,31 +1,54 @@
+/*
+ * Course: Problem Solving and Programming under Pressure (DD2458)
+ * Type: Laboratory exercises
+ * Authors:
+ * - Eduardo Rodes Pastor (9406031931)
+ * - Florian Cassayre (980703T092)
+ */
 package se.kth.popup.lab2.shortestpath;
 
 import java.util.List;
+import static java.lang.Integer.*;
 
 public final class FloydWarshall {
     private FloydWarshall() {}
 
+    /**
+     * Computes the shortest distances for all pairs of vertices.
+     * Negative weights are allowed and negative cycles will be detected.
+     * Produces the same result as {@link BellmanFord} but slightly more efficiently for all pairs.
+     * @param n the number of vertices
+     * @param edges the edges
+     * @return a solution containing the costs and the paths;
+     * a distance of <code>Integer.MAX_VALUE</code> indicates an impossible path
+     * while <code>Integer.MIN_VALUE</code> means that the cost of the path can be
+     * made arbitrarily low.
+     */
     public static int[][] allPairsShortestPaths(int n, List<Edge> edges) {
-        final int[][] matrix = new int[n][n];
+        final int[][] matrix = new int[n][n]; // The adjacency/result matrix
         for(int i = 0; i < n; i++)
             for(int j = 0; j < n; j++)
-                matrix[i][j] = i == j ? 0 : Integer.MAX_VALUE;
+                matrix[i][j] = i == j ? 0 : MAX_VALUE;
         for(Edge edge : edges)
-            matrix[edge.u][edge.v] = (edge.u == edge.v && edge.weight > 0) ? 0 : edge.weight;
+            if(edge.u != edge.v || edge.weight < 0) // Self loops must be 0 or negative
+                matrix[edge.u][edge.v] = Math.min(edge.weight, matrix[edge.u][edge.v]);
 
-        for(int k = 0; k < n; k++)
-            for(int i = 0; i < n; i++)
-                for(int j = 0; j < n; j++) {
-                    final int a = matrix[i][k], b = matrix[k][j],
-                            sum = a < Integer.MAX_VALUE && b < Integer.MAX_VALUE ? a + b : Integer.MAX_VALUE;
-                    matrix[i][j] = Math.min(matrix[i][j], sum);
-                }
-
+        // Applying the Floyd-Warshall recurrence
         for(int k = 0; k < n; k++)
             for(int i = 0; i < n; i++)
                 for(int j = 0; j < n; j++)
-                    if(matrix[k][j] < Integer.MAX_VALUE && matrix[j][j] < 0 && matrix[j][i] < Integer.MAX_VALUE)
-                        matrix[k][i] = Integer.MIN_VALUE;
+                    if(matrix[i][k] < MAX_VALUE && matrix[k][j] < MAX_VALUE)
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+
+        // Find all the negative cycles
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                for(int k = 0; k < n; k++) {
+                    if(matrix[i][j] == MAX_VALUE)
+                        break;
+                    if(matrix[i][k] < MAX_VALUE && matrix[k][j] < MAX_VALUE && matrix[k][k] < 0)
+                        matrix[i][j] = MIN_VALUE;
+                }
 
         return matrix;
     }
